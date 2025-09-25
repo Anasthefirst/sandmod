6f85764ee2eb18faaaf5f5b147f7c92b8a8cacdd
Anasthefirst <Anasrefa2@gmail.com>
2025-09-25 14:20:42 -0500

    Update 01mod.js

--- 01mod.js
+++ 01mod.js
@@ -1,15 +1,4 @@
-elements.grapesodagas = {
-    color: "#B99FEA",
-    behavior: behaviors.GAS, 
-    category: "gases",
-    state: "gas",
-    density: 1100,
-    stateLow: "grapesoda",
-    tempLow: 256,
-    temp: 257,
-  }
-
-  elements.water.color = "#eb4034";
+elements.water.color = "96fffd";
 
   elements.anas = {
     color: "#eb4034",
